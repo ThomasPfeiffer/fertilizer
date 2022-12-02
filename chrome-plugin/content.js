31af6e2dc8b4c65802edcab0b0d2a5079fd2bf61
André Schmitz <andre.schmitz@lise.de>
2022-12-02 16:10:43 +0100

    method to insert element

--- chrome-plugin/content.js
+++ chrome-plugin/content.js
@@ -16,6 +16,36 @@ function run() {
   });
 }
 
+// Idee verworfen
+function getAllHtmlTimesheetEntries() {
+  const allTimesheets = document.getElementById("day-view-entries")
+  const tbody = allTimesheets.getElementsByTagName("tbody")[0]
+
+  for(let i = 0; i < tbody.children.length - 1; ++i){
+    // const starttimeOfNextElem = tbody.children.item(i + 1).getElementsByClassName("entry-timestamp-start")[0].textContent
+    // const starttime = tbody.children.item(i).getElementsByClassName("entry-timestamp-start")[0].textContent
+
+
+  }
+}
+
+function addTableRowToTimesheet(elementToAppend, type){
+  const trElem = document.createElement("tr")
+  trElem.setAttribute("colspan", "2")
+  trElem.setAttribute("id", "luegge_" + elementToAppend.getAttribute("id").split('_').pop())
+  const tdInTr = document.createElement("td")
+  tdInTr.classList.add("lueggeBox-td")
+  tdInTr.setAttribute("colspan", "3")
+  const innerDivOfTr = document.createElement("div")
+  innerDivOfTr.classList.add("lueggebox-" + type)
+  innerDivOfTr.textContent = "soos"
+  trElem.append(tdInTr)
+  tdInTr.append(innerDivOfTr)
+
+  elementToAppend.after(trElem)
+  return trElem
+}
+
 const checkTimes = () => {
   const dataIsland = window.document.querySelector("#timesheet-data-island");
   const timesheetString = dataIsland.textContent;
@@ -23,7 +53,7 @@ const checkTimes = () => {
   console.log(timesheet.week_of);
 };
 
-setInterval(() => checkTimes(), 1000);
+setInterval(() => addTableRowToTimesheet(document.getElementById("timesheet_day_entry_1944091275"), "pause"), 1000);
 
 window.addEventListener(
   "load",

c98b90a60242c6c6da9628b7a80f8764034f7f86
Thomas Pfeiffer <thomas.pfeiffer@lise.de>
2022-12-05 13:49:21 +0100

    Removed unused files

--- chrome-plugin/popup.js
+++
@@ -1,9 +0,0 @@
-async function cats() {
-  const [currentTab] = await chrome.tabs.query({
-    active: true,
-    currentWindow: true,
-  });
-  chrome.tabs.sendMessage(currentTab.id ?? 0, { command: "cats" });
-}
-
-document.getElementById("btn-cats")?.addEventListener("click", cats);

367974282bee43d84985c5b895c82354a19d3b1d
Thomas Pfeiffer <thomas.pfeiffer@lise.de>
2022-12-28 10:47:36 +0100

    Refactored manfiest and removed unused parts

--- chrome-plugin-new/fertilizer/src/manifest.ts
+++ chrome-plugin-new/fertilizer/src/manifest.ts
@@ -1,10 +1,14 @@
 import pkg from "../package.json";
 
 const sharedManifest = {
+  author: pkg.author,
+  description: pkg.description,
+  name: pkg.displayName ?? pkg.name,
+  version: pkg.version,
   content_scripts: [
     {
       js: ["src/entries/contentScript/primary/main.ts"],
-      matches: ["*://*/*"],
+      matches: ["https://*.harvestapp.com/time/*"],
     },
   ],
   icons: {
@@ -19,12 +23,8 @@ const sharedManifest = {
     256: "icons/256.png",
     512: "icons/512.png",
   },
-  options_ui: {
-    page: "src/entries/options/index.html",
-    open_in_tab: true,
-  },
   permissions: [],
-};
+} satisfies Partial<chrome.runtime.ManifestV2 | chrome.runtime.ManifestV3>;
 
 const browserAction = {
   default_icon: {
@@ -37,39 +37,20 @@ const browserAction = {
 };
 
 const ManifestV2 = {
-  ...sharedManifest,
-  background: {
-    scripts: ["src/entries/background/script.ts"],
-    persistent: false,
-  },
   browser_action: browserAction,
-  options_ui: {
-    ...sharedManifest.options_ui,
-    chrome_style: false,
-  },
-  permissions: [...sharedManifest.permissions, "*://*/*"],
-};
+} satisfies Partial<chrome.runtime.ManifestV3>;
 
 const ManifestV3 = {
-  ...sharedManifest,
   action: browserAction,
-  background: {
-    service_worker: "src/entries/background/serviceWorker.ts",
-  },
   host_permissions: ["*://*/*"],
-};
+} satisfies Partial<chrome.runtime.ManifestV3>;
 
-export function getManifest(manifestVersion: number): chrome.runtime.ManifestV2 | chrome.runtime.ManifestV3 {
-  const manifest = {
-    author: pkg.author,
-    description: pkg.description,
-    name: pkg.displayName ?? pkg.name,
-    version: pkg.version,
-  };
-
+export function getManifest(
+  manifestVersion: number
+): chrome.runtime.ManifestV2 | chrome.runtime.ManifestV3 {
   if (manifestVersion === 2) {
     return {
-      ...manifest,
+      ...sharedManifest,
       ...ManifestV2,
       manifest_version: manifestVersion,
     };
@@ -77,7 +58,7 @@ export function getManifest(manifestVersion: number): chrome.runtime.ManifestV2 
 
   if (manifestVersion === 3) {
     return {
-      ...manifest,
+      ...sharedManifest,
       ...ManifestV3,
       manifest_version: manifestVersion,
     };
